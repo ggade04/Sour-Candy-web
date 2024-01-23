a3ce3811e94fe6ecd8f255144877765b0d62570a
Raviporngg <152627459+ggade04@users.noreply.github.com>
2024-01-24 02:16:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+
 
 document.querySelector('#search-icon').onclick = () =>{
     document.querySelector('#search-form').classList.toggle('active');
 }
-
+ 
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
 }
@@ -56,3 +53,10 @@ var swiper = new Swiper(".review-slider", {
         },
     },
 });
+
+renderProdcuts();
+//Add to cart
+function addToCart(id){
+    console.log(id);
+}
+
